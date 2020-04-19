baf2d81fcfd35528df662018515e5c1284becbea
Igor Trofimov <iigortrofimov@yandex.ru>
2020-04-19 12:32:22 +0300

    first

---
+++ src/test/java/ru/rogi/socialnet/SocialnetApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.rogi.socialnet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialnetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
